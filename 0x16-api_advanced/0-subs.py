1b6f5600741def04e13a72f2f5c9257e4c6881cf
eujoshSE <eujosh.w@gmail.com>
2024-06-13 05:37:09 -0700

    file updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/dev/api/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
